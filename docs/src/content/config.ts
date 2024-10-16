d7b7ed49b2ffeb5a7aefdc9bf11b9165d71c7043
Fevol <8179397+Fevol@users.noreply.github.com>
2024-10-16 15:10:15 +0200

    refactor: renamed pageSchema export to pageThemeObsidianSchema chore: bump starlight-site-graph version

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,11 +1,11 @@
 import { docsSchema } from '@astrojs/starlight/schema';
 import { defineCollection } from 'astro:content';
-import { pageSchema } from 'starlight-theme-obsidian/schema';
+import { pageThemeObsidianSchema } from 'starlight-theme-obsidian/schema';
 
 export const collections = {
 	docs: defineCollection({
 		schema: docsSchema({
-			extend: context => pageSchema(context),
+			extend: pageThemeObsidianSchema,
 		}),
 	}),
 };
