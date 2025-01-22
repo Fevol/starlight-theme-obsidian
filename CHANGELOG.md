fa5e4123ffee89e155f009d65ff202387c8159f9
Fevol <8179397+fevol@users.noreply.github.com>
2025-01-22 14:45:01 +0100

    chore: bump starlight-site-graph dependency fix: ignore incompatible type issue of schema docs: fix date

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## [0.1.0](https://github.com/Fevol/starlight-theme-obsidian/releases/tag/0.1.0) (2025/01/19)
+## [0.1.0](https://github.com/Fevol/starlight-theme-obsidian/releases/tag/0.1.0) (2025/01/22)
 
 ### Upgrades
 
--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,13 +1,14 @@
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
-import { defineCollection } from 'astro:content';
+import {type BaseSchema, defineCollection} from 'astro:content';
 import { pageThemeObsidianSchema } from 'starlight-theme-obsidian/schema';
 
 export const collections = {
 	docs: defineCollection({
 		loader: docsLoader(),
 		schema: docsSchema({
-			extend: pageThemeObsidianSchema,
+			// TODO: Identify type conflict problem
+			extend: pageThemeObsidianSchema as BaseSchema,
 		}),
 	}),
 };
