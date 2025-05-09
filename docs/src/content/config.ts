7c0b91d19bb4a7a78054c5808496acb6b35cebba
Fevol <8179397+fevol@users.noreply.github.com>
2025-05-09 02:03:41 +0200

    fix: build issue

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,14 +1,13 @@
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
-import {type BaseSchema, defineCollection} from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { pageThemeObsidianSchema } from 'starlight-theme-obsidian/schema';
 
 export const collections = {
 	docs: defineCollection({
 		loader: docsLoader(),
 		schema: docsSchema({
-			// TODO: Identify type conflict problem
-			extend: pageThemeObsidianSchema as BaseSchema,
+			extend: pageThemeObsidianSchema,
 		}),
 	}),
 };
